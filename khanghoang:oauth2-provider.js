7370a1b0c11ecad895add20e471fec3b7e55b796
Triệu Khang <hoangtrieukhang@MBAKhang-4.local>
2014-12-28 00:46:13 +0700

    new way to generate token

--- khanghoang:oauth2-provider.js
+++ khanghoang:oauth2-provider.js
@@ -1,22 +1,37 @@
 Token = new Mongo.Collection('tokens')
 
-if(Meteor.isServer) {
-  Meteor.methods({
+KHOAuth2Provider = function(options) {
+  options = options || {}
+}
+
+// if(Meteor.isServer) {
+  KHOAuth2Provider.prototype =  {
     exchange: function(username) {
 
-      // cryto encryte
+      // crypto encryte
+      var secondsInDay = 3600 * 24
+      debugger
 
+      // we also need session id
+      var sessionID = Meteor.uuid()
 
-      // check if they are correct
+      var timeNow = Math.floor(Date.now()/1000)
+      var expire = timeNow + secondsInDay
 
-      // return token
+      // random
+      var random = Math.floor(Math.random() * 99999)
+
+      // data string
+      var dataString = "session_id" + sessionID + "&create_time=" + timeNow + '&expire_time=' + expire
+
+      var hmac = Npm.require('crypto-js').HmacSHA1(dataString, 'username')
+
       // return Npm.require('crypto-js').HmacSHA1('username', 'password')
-      var token = Npm.require('crypto-js').HmacSHA1('username', 'password')
+      var token = hmac.toString()
+      console.log(token)
 
-      console.log(token.toString())
-
-      return token.toString()
+      return token
     }
-  })
-}
+  }
+// }
 
